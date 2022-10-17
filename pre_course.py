e8fc100fe79dd4a0f0f9fd46836883700270906c
Alistair Cooke <alcooke@Alistairs-MacBook-Air.local>
2022-10-17 18:24:43 +0100

    first commit

---
+++ pre_course.py
@@ -0,0 +1,5 @@
+
+def codeclan_experience(day, message):
+    print("Today was day " + str(day) + " at CodeClan and " + message)
+
+codeclan_experience(1, "we covered the command line as well as git and github")
